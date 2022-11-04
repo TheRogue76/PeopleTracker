8d0161fdfd70d0ea5bcc8516d2195c769d19f3c8
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2022-11-04 13:18:32 +0100

    Initial Commit

---
+++ Challenge6/Challenge6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6App.swift
+//  Challenge6
+//
+//  Created by Parsa Nasirimehr on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
